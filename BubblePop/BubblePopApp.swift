99d33de563a1e9ad061410ec4d5ca6437dab88a7
hxycindyhxy <xinyi.hu-3@student.uts.edu.au>
2025-05-27 10:35:30 +1000

    -add the enviroments objects on App page.

--- BubblePop/BubblePopApp.swift
+++ BubblePop/BubblePopApp.swift
@@ -9,9 +9,16 @@ import SwiftUI
 
 @main
 struct BubblePopApp: App {
+    @StateObject private var timerVM = TimerViewModel()
+    @StateObject private var playerVM = PlayerViewModel()
+    @StateObject private var gameVM = GameViewModel()
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environmentObject(timerVM)
+                .environmentObject(playerVM)
+                .environmentObject(gameVM)
         }
     }
 }
